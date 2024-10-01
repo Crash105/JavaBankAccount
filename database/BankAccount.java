64e198991934defb7358c3b04b831a897a861096
Crash107 <brianm201720@gmail.com>
2024-09-30 22:13:47 -0400

    add files

---
+++ database/BankAccount.java
@@ -0,0 +1,10 @@
+package database;
+
+public class BankAccount {
+    public static void main(String[] args) {
+
+        Checking account1 = new Checking("Joe Smith");
+        Savings account2 = new Savings("Jane Doe");
+
+    }
+}
