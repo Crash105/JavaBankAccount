49732a58c2ab7b7ccd49ff4e5bd0045c93d6c358
Crash107 <brianm201720@gmail.com>
2024-10-02 11:53:45 -0400

    add files

--- database/Savings.java
+++ database/Savings.java
@@ -1,5 +1,7 @@
 package database;
 
+import java.util.Random;
+
 public class Savings extends Account {
 
     int safetyDepositID;
@@ -8,13 +10,23 @@ public class Savings extends Account {
     public Savings(String name, String SSN, int deposit) {
         super(name, SSN, deposit);
         accountNumber = "1" + accountNumber;
-
+        setSafetyBox();
     }
 
     public String showInfo() {
         System.out.println(super.showInfo());
         System.out.println("*************************");
-        return "ACCOUNT TYPE: SAVING" + "\nSavings Account Number: " + accountNumber;
+        return "ACCOUNT TYPE: SAVING" + "\nSavings Account Number: " + accountNumber + "\nSafety Deposit Box ID: "
+                + safetyDepositID + "\nSafety Deposot Box Key: " + safetyDepositKey;
+    }
+
+    private void setSafetyBox() {
+        Random rand = new Random();
+        int rand_int = rand.nextInt(1000);
+        this.safetyDepositID = rand_int;
+        int rand_int1 = rand.nextInt(10000);
+        this.safetyDepositKey = rand_int1;
+
     }
 
 }
