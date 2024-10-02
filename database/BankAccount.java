cb99f387e2d82a9bac2c6b9cba00a9bdb51b1d39
Crash107 <brianm201720@gmail.com>
2024-10-02 13:59:53 -0400

    add files

--- database/BankAccount.java
+++ database/BankAccount.java
@@ -4,8 +4,11 @@ public class BankAccount {
     public static void main(String[] args) {
 
         Checking account1 = new Checking("Joe Smith", "123456789", 200);
+        account1.deposit(200);
+        account1.withdraw(100);
+        account1.transfer("Brokerage", 50);
         System.out.println(account1.showInfo());
-        Savings account2 = new Savings("Joe Smith", "123456789", 200);
+        Savings account2 = new Savings("Joe Smith", "123456789", 400);
         System.out.println(account2.showInfo());
 
     }
