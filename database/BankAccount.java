45c8c2b21df499a0a7aa0a137ab7c29fabf624a2
Crash107 <brianm201720@gmail.com>
2024-10-02 17:16:19 -0400

    add files

--- database/BankAccount.java
+++ database/BankAccount.java
@@ -1,15 +1,31 @@
 package database;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class BankAccount {
     public static void main(String[] args) {
 
-        Checking account1 = new Checking("Joe Smith", "123456789", 200);
-        account1.deposit(200);
-        account1.withdraw(100);
-        account1.transfer("Brokerage", 50);
-        System.out.println(account1.showInfo());
-        Savings account2 = new Savings("Joe Smith", "123456789", 400);
-        System.out.println(account2.showInfo());
+        String file = "C:\\Users\\Brian\\BankAccountApplication\\JavaBankAccount\\database\\NewBankAccounts.csv";
+
+        List<String[]> newcustomers = database.utilities.CSV.read(file);
+
+        for (String[] newcustomer : newcustomers) {
+            System.out.println("New Account");
+            System.out.println(newcustomer[0]);
+            System.out.println(newcustomer[1]);
+            System.out.println(newcustomer[2]);
+            System.out.println(newcustomer[3]);
+
+        }
+
+        // Checking account1 = new Checking("Joe Smith", "123456789", 200);
+        // account1.deposit(200);
+        // account1.withdraw(100);
+        // account1.transfer("Brokerage", 50);
+        // System.out.println(account1.showInfo());
+        // Savings account2 = new Savings("Joe Smith", "123456789", 400);
+        // System.out.println(account2.showInfo());
 
     }
 }
